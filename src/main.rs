a42b2697ddb6d9e258159a0117e9bed198d9746f
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2020-02-15 18:16:25 +0100

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 extern crate clap;
 extern crate ignore;
+extern crate grep;
 
 use clap::{App, Arg};
 use ignore::WalkBuilder;
 
-fn search_path(path: &str) {
+use grep::regex::RegexMatcher;
+use grep::matcher::{LineTerminator, Matcher};
+use grep::searcher::{SearcherBuilder, Searcher, SinkMatch, Sink};
+
+fn search_path(pattern: &str, path: &str) -> Result<(), Box<std::error::Error>> {
     let builder = WalkBuilder::new(path);
+
+    let matcher = RegexMatcher::new_line_matcher(&pattern)?;
 
     builder.build_parallel().run(|| {
         Box::new(move |result| {
+            let matcher = matcher.clone();
+            let mut searcher = SearcherBuilder::new()
+                .line_terminator(LineTerminator::byte(b'\n'))
+                .line_number(true)
+                .multi_line(false)
+                .build();
+
             let dir_entry = match result {
                 Err(err) => {
                     eprintln!("{}", err);
@@ -24,9 +38,33 @@ fn search_path(path: &str) {
                     entry
                 }
             };
-            return ignore::WalkState::Continue;
+            let mut match_count = 0;
+            let result = searcher.search_path(
+                &matcher,
+                dir_entry.path(),
+                MatchesSink(|_, sink_match| {
+                    matcher.find_iter(sink_match.bytes(), |_| {
+                        match_count += 1;
+                        true
+                    })?;
+                    Ok(true)
+                }),
+            );
+
+            ignore::WalkState::Continue
         })
     });
+
+    Ok(())
+}
+
+pub struct MatchesSink<F>(pub F) where F: FnMut(&Searcher, &SinkMatch) -> Result<bool, std::io::Error>;
+
+impl<F> Sink for MatchesSink<F> where F: FnMut(&Searcher, &SinkMatch) -> Result<bool, std::io::Error> {
+    type Error = std::io::Error;
+    fn matched(&mut self, searcher: &Searcher, sink_match: &SinkMatch) -> Result<bool, std::io::Error> {
+        (self.0)(searcher, sink_match)
+    }
 }
 
 fn main() {
@@ -42,5 +80,5 @@ fn main() {
 
     let path = matches.value_of("PATH").unwrap();
 
-    search_path(path);
+    search_path("dupa", path);
 }

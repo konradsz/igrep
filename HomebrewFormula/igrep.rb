e259b6f3c67a6365bad1e6b8fc26db36a34b25cb
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-04-24 19:13:28 +0200

    v0.5.0

--- HomebrewFormula/igrep.rb
+++ HomebrewFormula/igrep.rb
@@ -1,9 +1,9 @@
 class Igrep < Formula
-  version "0.4.0"
+  version "0.5.0"
   desc "Interactive Grep"
   homepage "https://github.com/konradsz/igrep"
   url "https://github.com/konradsz/igrep/releases/download/v#{version}/igrep-v#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "89b9e6ab2561dbc0b236ef41702aacd73fb78bc9492da97e1b627e2856f15202"
+  sha256 "968f83152d9b00ede61e6336baa97193bf778ef36fb8d7a3fe8870e81c6d646b"
 
   def install
     bin.install "ig"

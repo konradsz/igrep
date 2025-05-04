74c2b4090f60eea36bab3260693d9893277e0d3c
Tetrix <tetrix@ixpa.de>
2025-05-04 09:20:21 +0200

    display <tab> as 2 <spaces> (#77)

--- src/ig/file_entry.rs
+++ src/ig/file_entry.rs
@@ -11,11 +11,26 @@ impl FileEntry {
     pub fn new(name: String, matches: Vec<GrepMatch>) -> Self {
         Self(
             std::iter::once(EntryType::Header(name))
-                .chain(
-                    matches
-                        .into_iter()
-                        .map(|m| EntryType::Match(m.line_number, m.text, m.match_offsets)),
-                )
+                .chain(matches.into_iter().map(|m| {
+                    let mut text = String::new();
+                    let mut ofs = m.match_offsets;
+                    let mut pos = 0;
+                    for c in m.text.chars() {
+                        pos += 1;
+                        if c != '\t' {
+                            text.push(c);
+                        } else {
+                            text.push_str("  ");
+                            for p in &mut ofs {
+                                if p.0 >= pos {
+                                    p.0 += 1;
+                                    p.1 += 1;
+                                }
+                            }
+                        }
+                    }
+                    EntryType::Match(m.line_number, text, ofs)
+                }))
                 .collect(),
         )
     }
